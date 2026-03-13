d63d3f6d14d38efe8e7487bc7f9f58b4775b5f4c
Andresp1073 <andresmauriciope1073@gmail.com>
2026-03-13 11:14:51 -0500

    Limpiar cache service worker

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,5 @@
-self.addEventListener("install", event => {
-  self.skipWaiting()
-})
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.map(key => caches.delete(key))
-    ))
-  )
-})
-
-self.addEventListener("fetch", event => {
-  event.respondWith(fetch(event.request))
-})
+self.addEventListener("install", () => self.skipWaiting())
+self.addEventListener("activate", event => event.waitUntil(
+  caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+))
+self.addEventListener("fetch", event => event.respondWith(fetch(event.request)))
